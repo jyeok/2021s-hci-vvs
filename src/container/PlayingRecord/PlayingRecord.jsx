8f72d677f7a0108eb0a8b306ab46b3f4f47f0a84
standtwopercent <81569368+standtwopercent@users.noreply.github.com>
2021-05-28 02:04:42 +0900

    Modified TextField, Message Props
    - modified hard coding into property receiving

--- src/container/PlayingRecord/PlayingRecord.jsx
+++ src/container/PlayingRecord/PlayingRecord.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, TextField } from "@material-ui/core";
 import { ArrowBack } from "@material-ui/icons";
+import HelpIcon from "@material-ui/icons/Help";
 import { PropTypes } from "prop-types";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
@@ -11,17 +12,14 @@ import {
   Message,
 } from "@chatscope/chat-ui-kit-react";
 
-import { Media, Player, controls } from "react-media-player";
-
 const PlayingRecord = (prop) => {
-  const { name, createdAt, messageDisplayed, record } = prop;
-  const { PlayPause, MuteUnmute } = controls;
+  const { content, isMine, start } = prop;
   return (
     <div>
       <Grid container padding={15} style={{ border: "1px solid" }}>
         <Grid item xs={12} style={{ borderBottom: "0.5px solid" }}>
           <ArrowBack />
-          {name} {createdAt}
+          {}
           <button type="button">전체 요약</button>
           <button type="button">선택 요약</button>
         </Grid>
@@ -32,19 +30,10 @@ const PlayingRecord = (prop) => {
                 <MessageList>
                   <Message
                     model={{
-                      message: messageDisplayed,
-                      sentTime: "2020/05/04",
+                      message: content,
+                      sentTime: start,
                       sender: "Susan",
-                      direction: "incoming",
-                      position: "single",
-                    }}
-                  />
-                  <Message
-                    model={{
-                      message: "ㄴㅏ는 배가 고프다",
-                      sentTime: "just now",
-                      sender: "me",
-                      direction: "outgoing",
+                      direction: isMine ? "incoming" : "outgoing",
                       position: "single",
                     }}
                   />
@@ -54,27 +43,19 @@ const PlayingRecord = (prop) => {
           </div>
         </Grid>
         <Grid item xs={12} style={{ borderBottom: "0.5px solid" }}>
-          <div>
-            <Media>
-              <div className="media">
-                <div className="media-player">
-                  <Player src={record} />
-                </div>
-                <div className="media-controls">
-                  <PlayPause />
-                  <MuteUnmute />
-                </div>
-              </div>
-            </Media>
-          </div>
+          <div>여긴 재생버튼</div>
         </Grid>
-        <Grid>
-          <div>
-            질문하기
-            <textarea row="1" style={{ width: "180%" }}>
-              여기에 질문을 입력하시면 대답해 드립니다.
-            </textarea>
-          </div>
+        <Grid item xs={1}>
+          <HelpIcon fontSize="large" />
+        </Grid>
+        <Grid item xs={11}>
+          {" "}
+          <TextField
+            id="Question"
+            row="1"
+            label="여기에 질문을 입력해보세요."
+            fullWidth
+          />
         </Grid>
       </Grid>
     </div>
@@ -84,15 +65,20 @@ const PlayingRecord = (prop) => {
 export default PlayingRecord;
 
 PlayingRecord.defaultProps = {
-  name: "녹음명",
-  createdAt: "녹음 날짜",
-  messageDisplayed: "can you play this sample message?",
-  record: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  content: "Does this work?",
+  isMine: 1,
+  start: "03:45",
 };
 
 PlayingRecord.PropType = {
-  name: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
-  messageDisplayed: PropTypes.string.isRequired,
-  record: PropTypes.string.isRequired,
+  PlayingRecord: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    content: PropTypes.string.isRequired,
+    isMine: PropTypes.number,
+    isHighlighted: PropTypes.bool.isRequired,
+    isModified: PropTypes.bool,
+    reliability: PropTypes.number,
+    start: PropTypes.string.isRequired,
+    end: PropTypes.string.isRequired,
+  }),
 };
