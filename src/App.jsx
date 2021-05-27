7da41bd2011ed884ec80cffe70f1fb7036df102c
jyeok <jyeokchoi@gmail.com>
2021-05-28 00:55:49 +0900

    hotfix: remove duplicated component declaration in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import "./index.css";
-
-import Explorer from "explorer/Explorer";
-import WhileRecording from "WhileRecording/WhileRecording";
-import PlayingRecord from "container/PlayingRecord/PlayingRecord";
 
 import Explorer from "explorer/Explorer";
 import WhileRecording from "WhileRecording/WhileRecording";
@@ -14,9 +9,6 @@ function App() {
     <div className="App">
       <Route path="/" exact component={Explorer} />
       <Route path="/recording" exact component={WhileRecording} />
-      <Explorer />
-      <WhileRecording />
-      <PlayingRecord />
     </div>
   );
 }
