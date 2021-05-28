6d4c0538cbcf00faa63e8a629b353fddc57d59a2
jyeok <jyeokchoi@gmail.com>
2021-05-28 22:52:45 +0900

    modify preview layout

--- src/container/Preview/Preview.jsx
+++ src/container/Preview/Preview.jsx
@@ -9,26 +9,26 @@ function Preview() {
     marginBottom: "8px",
   };
   const pborder = {
-    border: "0.5 px solid",
+    border: "0.5px solid",
   };
   return (
     <div>
-      <div style={{ margin: "3px", height: "400px" }}>
-        Preview
+      Preview
+      <div style={{ border: "1px solid", margin: "5px", height: "400px" }}>
         <MessageHolder />
       </div>
-      <div style={{ pstyle }}>
-        <Button type="button" style={{ pborder }}>
+      <div style={pstyle}>
+        <Button type="button" style={pborder}>
           편집
         </Button>
       </div>
-      <div style={{ pstyle }}>
-        <Button type="button" style={{ pborder }}>
+      <div style={pstyle}>
+        <Button type="button" style={pborder}>
           재생
         </Button>
       </div>
-      <div style={{ pstyle }}>
-        <Button type="button" style={{ pborder }}>
+      <div style={pstyle}>
+        <Button type="button" style={pborder}>
           잠금
         </Button>
       </div>
