6ef21b71829d810a87513184b22310cf227ec10e
standtwopercent <81569368+standtwopercent@users.noreply.github.com>
2021-06-01 02:28:15 +0900

    adjusted designs

--- src/WhileRecording/WhileRecording.jsx
+++ src/WhileRecording/WhileRecording.jsx
@@ -1,10 +1,12 @@
-import React, { PureComponent } from "react";
-import { Grid, TextField } from "@material-ui/core";
-import { Delete, SaveAlt } from "@material-ui/icons";
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+import { useHistory } from "react-router-dom";
+import { Grid, TextField, IconButton } from "@material-ui/core";
+import { Delete, SaveAlt, ArrowBack } from "@material-ui/icons";
 import { ReactMic } from "react-mic";
 import RecordingMessage from "../container/RecordingMessage/RecordingMessage";
 
-class WhileRecording extends PureComponent {
+class WhileRecording extends Component {
   constructor(props) {
     super(props);
 
@@ -37,40 +39,33 @@ class WhileRecording extends PureComponent {
 
   render() {
     const { record } = this.state;
+
     return (
       <Grid container padding={15} style={{ border: "1px solid" }}>
         <Grid
           item
           xs={12}
-          style={({ borderBottom: "0.5px solid" }, { height: "100px" })}
+          style={({ height: "150px" }, { borderBottom: "0.5px solid" })}
         >
-          <ReactMic
-            record={record}
-            className="sound-wave"
-            onStop={this.onStop}
-            onData={this.onData}
-            strokeColor="#000000"
-            visualSetting="False"
-          />
-          <button onClick={this.startRecording} type="button">
-            Start
-          </button>
-          <button onClick={this.stopRecording} type="button">
-            Stop
-          </button>
+          <ArrowBack fontsize="large" />
+          <TextField label="제목" />
         </Grid>
         <Grid
           item
           xs={8}
-          style={({ height: "500px" }, { borderRight: "0.5px solid" })}
+          style={({ height: "600px" }, { borderRight: "0.5px solid" })}
         >
-          <Grid style={{ height: "600px" }}>
+          <Grid style={({ height: "5500px" }, { borderBottom: "0.5px solid" })}>
             <RecordingMessage />
-            <Delete />
-            <SaveAlt />
+            <Delete fontSize="large" />
+            <SaveAlt fontSize="large" />
           </Grid>
         </Grid>
-        <Grid item xs={4} style={{ height: "500px" }}>
+        <Grid
+          item
+          xs={4}
+          style={({ height: "600px" }, { borderBottom: "0.5px solid" })}
+        >
           <div>메모</div>
           <TextField
             variant="outlined"
@@ -91,6 +86,25 @@ class WhileRecording extends PureComponent {
             defaultValue="키워드가 이곳에 나타납니다. 여기를 눌러 수정도 가능합니다."
             fullWidth
           />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          style={({ borderBottom: "0.5px solid" }, { height: "120px" })}
+        >
+          <ReactMic
+            record={record}
+            onStop={this.onStop}
+            onData={this.onData}
+            strokeColor="#000000"
+            visualSetting="sinewave"
+          />
+          <button onClick={this.startRecording} type="button">
+            Start
+          </button>
+          <button onClick={this.stopRecording} type="button">
+            Stop
+          </button>
         </Grid>
       </Grid>
     );
--- src/container/PlayingRecord/PlayingRecord.jsx
+++ src/container/PlayingRecord/PlayingRecord.jsx
@@ -45,10 +45,15 @@ const PlayingRecord = () => {
 
   return (
     <Grid container padding={15} style={{ border: "1px solid" }}>
-      <Grid item xs={12} style={{ borderBottom: "0.5px solid" }}>
+      <Grid
+        item
+        xs={12}
+        style={({ borderBottom: "0.5px solid" }, { height: "50px" })}
+      >
         <ArrowBack onClick={goBack} />
         <button
           type="button"
+          style={{ float: "right" }}
           onClick={() => {
             // eslint-disable-next-line no-alert
             alert(textrank.getSummarizedThreeText());
@@ -56,12 +61,14 @@ const PlayingRecord = () => {
         >
           전체 요약
         </button>
-        <button type="button">선택 요약</button>
+        <button type="button" style={{ float: "right" }}>
+          선택 요약
+        </button>
       </Grid>
       <Grid
         item
         xs={12}
-        style={{ borderBottom: "0.5px solid", height: "600px" }}
+        style={{ borderBottom: "0.5px solid", height: "500px" }}
       >
         {data.recordById &&
           data.recordById.content.map((e, i) => (
