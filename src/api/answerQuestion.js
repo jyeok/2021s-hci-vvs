60a964bae94fb7f5e4dfb0c7b958244c0b45c8cd
standtwopercent <81569368+standtwopercent@users.noreply.github.com>
2021-05-31 21:49:36 +0900

    changed seed data
    - update test data
    - changed console.log to alert

--- src/api/answerQuestion.js
+++ src/api/answerQuestion.js
@@ -21,8 +21,8 @@ export function answerQuestion(sampleParagraph, sampleQuestion) {
         const { answer } = data.result;
         const answerIndex = data.result.answer_index;
         const answersToQuestion = [answer, answerIndex];
-        // eslint-disable-next-line no-console
-        console.log(answersToQuestion);
+        // eslint-disable-next-line no-alert
+        alert(answer);
         return answersToQuestion;
       })
       // eslint-disable-next-line no-console
